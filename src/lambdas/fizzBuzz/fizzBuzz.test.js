1ce05d3cfcf5ed7e52b1c17d0f8ea8dd2a134ece
k2works <kakimomokuri@gmail.com>
2018-05-15 14:20:17 +0900

    FizzBuzz開発
    リファクタリング

--- src/lambdas/fizzBuzz/fizzBuzz.test.js
+++ src/lambdas/fizzBuzz/fizzBuzz.test.js
@@ -2,20 +2,23 @@ import * as chai from "chai";
 import FizzBuzz from "./fizzBuzz";
 
 describe("A FizzBuzz generator", () => {
+    var fizzBuzz;
+
+    beforeEach(() => {
+        fizzBuzz = new FizzBuzz();
+    });
+
     it("should return the number 1 when 1 is played",() => {
-        var fizzBuzz = new FizzBuzz(1);
         var result = fizzBuzz.generate(1);
         chai.expect(result).to.equal(1);
     });
 
     it("should return the number 2 when 2 is played", () => {
-        var fizzBuzz = new FizzBuzz();
         var result = fizzBuzz.generate(2);
         chai.expect(result).to.equal(2);
     });
 
     it('should return "Fizz" when 3 is played', () => {
-        var fizzBuzz = new FizzBuzz();
         var result = fizzBuzz.generate(3);
         chai.expect(result).to.equal('Fizz');
     } )
