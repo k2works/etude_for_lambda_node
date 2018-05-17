a256cca1a26d7e38a038b17088895cf470860210
k2works <kakimomokuri@gmail.com>
2018-05-17 11:33:24 +0900

    運用 npmスクリプト不備修正

--- src/app/server/render-app.js
+++ src/app/server/render-app.js
@@ -1,5 +1,5 @@
-import { APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT } from '../shared/config'
-import { isProd } from '../shared/util'
+import { APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT } from "../shared/config";
+import { isProd } from "../shared/util";
 
 const renderApp = (title) =>
     `<!doctype html>
@@ -13,6 +13,6 @@ const renderApp = (title) =>
     <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/app/bundle.js"></script>
   </body>
 </html>
-`
+`;
 
-export default renderApp
+export default renderApp;
--- src/lambdas/robots/index.js
+++ src/lambdas/robots/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import "babel-polyfill";
 import * as cassava from "cassava";
 
@@ -25,7 +26,7 @@ router.route("/robots")
     .handler(evt => {
         return {
             body: Object.keys(robots).map(key => robots[key])
-        }
+        };
     });
 
 router.route("/robots/{robot}")
@@ -62,7 +63,7 @@ router.route("/robots/{robot}")
         return {
             statusCode: cassava.httpStatusCode.success.NO_CONTENT,
             body: null
-        }
+        };
     });
 
 router.route("/robots/{robot}")
@@ -77,7 +78,7 @@ router.route("/robots/{robot}")
         return {
             statusCode: cassava.httpStatusCode.success.NO_CONTENT,
             body: null
-        }
+        };
     });
 
 /**
