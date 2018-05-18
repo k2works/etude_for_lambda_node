34193ecdc9fdba2dc2d23c49db8969e08f452428
k2works <kakimomokuri@gmail.com>
2018-05-18 13:33:17 +0900

    運用 本番アセットのパスを変更

--- src/app/shared/config.js
+++ src/app/shared/config.js
@@ -1,5 +1,5 @@
 export const WEB_PORT = process.env.PORT || 8000;
-export const STATIC_PATH = "/static";
+export const STATIC_PATH = "https://s3.amazonaws.com/etude-for-lambda-node-aws-serverless-express/assets";
 export const APP_NAME = "Hello App";
 
 export const WDS_PORT = 7000;
