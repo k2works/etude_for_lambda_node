5a641038e9737974269d11737a3583dd1454694b
k2works <kakimomokuri@gmail.com>
2018-05-23 16:35:09 +0900

    React開発
    Server-Side Rendering

--- src/app/server/app.js
+++ src/app/server/app.js
@@ -1,9 +1,8 @@
 import compression from "compression";
 import express from "express";
 
-import { APP_NAME, STATIC_PATH } from "../shared/config";
-import renderApp from "./render-app";
-import { helloEndpointRoute } from "../shared/routes";
+import routing from "./routing";
+import { STATIC_PATH } from "../shared/config";
 
 const app = express();
 
@@ -11,12 +10,6 @@ app.use(compression());
 app.use(STATIC_PATH, express.static("app"));
 app.use(STATIC_PATH, express.static("public"));
 
-app.get("/", (req, res) => {
-    res.send(renderApp(APP_NAME));
-});
-
-app.get(helloEndpointRoute(), (req, res) => {
-    res.json({ serverMessage: `Hello from the server! (received ${req.params.num})` });
-});
+routing(app);
 
 export default app;
